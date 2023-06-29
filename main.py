62cd8540a0e9bfb47d6111935bb6e3373dcbd714
MaxJameson <maxjameson1999@live.co.uk>
2023-06-29 16:01:45 +0100

    UI shows output + block /0 and operator stacking

--- main.py
+++ main.py
@@ -10,28 +10,66 @@ class MyGUI(QMainWindow):
         uic.loadUi("gui.ui", self)
         self.show()
 
-        self.currentNum = ""
+        self.currentExp = ""
 
         button_List = self.findChildren(QPushButton)
+        self.num_Buttons = []
+        self.operator_Buttons = []
 
         for button in button_List:
-            if button.text() != "=":
-                button.clicked.connect(lambda checked, btn=button: self.input(btn.text()))
+            if button.text() == "=":
+                button.clicked.connect(self.total)
+                self.operator_Buttons.append(button)
+            elif button.text() == "C":
+                button.clicked.connect(self.clear)
             else:
-                self.Btn_sum.clicked.connect(self.total)
+                button.clicked.connect(lambda checked, btn=button: self.input(btn.text()))
 
+                if button.text().isdigit():
+                    self.num_Buttons.append(button)
+                else:
+                    self.operator_Buttons.append(button)                
 
 
     # outputs a message box containing a users input
-    def input(self, num):
+    def input(self, userInput):
         
-        # NEED TO DISPLAY MUTITPLE OPERATOR INPUT
-        self.currentNum = self.currentNum + num
+        if len(self.currentExp) > 0:
 
+            # prevents division by zero
+            if self.currentExp[-1] != "/":
+                self.Btn_0.setEnabled(True)
+
+            # Prevents user from inputting two operators consecutively
+            if not (self.currentExp[-1].isdigit() or userInput.isdigit()):
+                return
+
+            # adds current input to expression
+            self.currentExp += userInput
+            self.outputLbl.setText(self.currentExp)
+
+            # prevents division by zero
+            if userInput == "/":
+                self.Btn_0.setEnabled(False)
+
+        # checks if first input is an operator        
+        elif userInput.isdigit():
+
+            # adds input tp expression
+            self.currentExp += userInput
+            self.outputLbl.setText(self.currentExp)
+
+    # sums up total
     def total(self):
-        test = self.currentNum
-        res = eval(test)
-        print(res)
+        res = eval(self.currentExp)
+        self.outputLbl.setText(self.currentExp + "=" + str(res))
+        self.currentExp = str(res)
+
+    # clears 
+    def clear(self):
+        self.Btn_0.setEnabled(True)
+        self.currentExp = ""
+        self.outputLbl.setText("")
 
 
 
